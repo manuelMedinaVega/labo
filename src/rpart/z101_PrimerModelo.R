b0deb6f74dfdc2295729d539fcad2852ccc9d47e
manuelMedinaVega <manuel.mv1191@gmail.com>
2022-08-17 23:06:01 -0300

    primeros modelos

--- src/rpart/z101_PrimerModelo.R
+++ src/rpart/z101_PrimerModelo.R
@@ -7,7 +7,10 @@ require("rpart")
 require("rpart.plot")
 
 #Aqui se debe poner la carpeta de la materia de SU computadora local
-setwd("D:\\gdrive\\UBA2022\\")  #Establezco el Working Directory
+setwd("C:\\Users\\manuel\\Documents\\m_d_m\\dmef")  #Establezco el Working Directory
+
+# Poner sus semillas
+semillas <- c(309367, 149521, 690467, 699191, 795931)
 
 #cargo el dataset
 dataset  <- fread("./datasets/competencia1_2022.csv")
