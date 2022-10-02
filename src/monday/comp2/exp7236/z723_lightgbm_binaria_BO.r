3fe3a5a4dd4eb7fccef3175acbeaf5285d68191a
manuelMedinaVega <manuel.mv1191@gmail.com>
2022-10-02 20:13:26 -0300

    exp7236: igual al anterior, pero con más hp

--- src/monday/comp2/exp7236/z723_lightgbm_binaria_BO.r
+++ src/monday/comp2/exp7236/z723_lightgbm_binaria_BO.r
@@ -37,10 +37,10 @@ hs <- makeParamSet(
          makeIntegerParam("min_data_in_leaf", lower=    500L   , upper=  8000L),#lower = 500L
          makeIntegerParam("num_leaves",       lower=   16L   , upper=  1024L),
          makeIntegerParam("envios",           lower= 5000L   , upper= 15000L),#busca el mejor punto de corte
-         makeNumericParam("bagging_fraction", lower=    0.2  , upper=    1.0)
-         #makeNumericParam("lambda_l1", lower=0.0, upper=5),
-         #makeNumericParam("lambda_l2", lower=0.0, upper=3),
-         #makeNumericParam("min_gain_to_split", lower=0.0, upper=0.1)
+         makeNumericParam("bagging_fraction", lower=    0.2  , upper=    1.0),
+         makeNumericParam("lambda_l1", lower=0.0, upper=5),
+         makeNumericParam("lambda_l2", lower=0.0, upper=3),
+         makeNumericParam("min_gain_to_split", lower=0.0, upper=0.1)
         )
 #max_bins
 #bagging_fraction: afecta como el extra_trees
@@ -52,9 +52,9 @@ hs <- makeParamSet(
 #  muy pronto esto se leera desde un archivo formato .yaml
 PARAM  <- list()
 
-PARAM$experimento  <- "HT7235"
+PARAM$experimento  <- "HT7236"
 
-PARAM$input$dataset       <- "./exp/FE7113/dataset_7113.csv.gz"
+PARAM$input$dataset       <- "./exp/FE7114/dataset_7114.csv.gz"
 PARAM$input$training      <- c( 202103 )
 
 PARAM$trainingstrategy$undersampling  <-  1.0   # un undersampling de 0.1  toma solo el 10% de los CONTINUA
@@ -134,9 +134,9 @@ EstimarGanancia_lightgbm  <- function( x )
                           feature_pre_filter= FALSE,
                           verbosity= -100,
                           max_depth=  -1,         # -1 significa no limitar,  por ahora lo dejo fijo
-                          min_gain_to_split= 0.0, #por ahora, lo dejo fijo
-                          lambda_l1= 0.0,         #por ahora, lo dejo fijo
-                          lambda_l2= 0.0,         #por ahora, lo dejo fijo
+                          #min_gain_to_split= 0.0, #por ahora, lo dejo fijo
+                          #lambda_l1= 0.0,         #por ahora, lo dejo fijo
+                          #lambda_l2= 0.0,         #por ahora, lo dejo fijo
                           max_bin= 31,            #por ahora, lo dejo fijo
                           num_iterations= 9999,   #un numero muy grande, lo limita early_stopping_rounds
                           force_row_wise= TRUE,   #para que los alumnos no se atemoricen con tantos warning
